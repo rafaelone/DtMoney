7dece477bf27392b35c944612c7a622af6f2a990
Rafael Sergio <rafael.sergio@lumis.com.br>
2021-03-17 14:55:24 -0300

    styled component configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { GlobalStyle } from "./styles/global"
+
 export function App() {
   return (
+    <>
+    <GlobalStyle/>
     <div className="App">
       <h1>hello world</h1>
     </div>
+    </>
   );
 }
 
