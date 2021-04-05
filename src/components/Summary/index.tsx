242f3061d8929f94f622c82a4a664532e0d0dfba
Rafael Sergio <rafael.sergio@lumis.com.br>
2021-04-05 20:22:59 -0300

    feat: funcionalidade de calcular extrato

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
 import totalImg from "../../assets/total.svg"
@@ -9,6 +9,24 @@ export function Summary(){
  
   const {transactions} = useContext(TransactionsContext)
 
+  const summary = useMemo(() => {
+    return transactions.reduce((acc, transaction) => {
+      if(transaction.type === 'deposit'){
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount
+      }else{
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount
+      } 
+  
+      return acc;
+    }, {
+      deposits: 0,
+      withdraws: 0,
+      total: 0
+    })
+  }, [transactions])
+
   return (
     <Container>
       <div>
@@ -16,21 +34,27 @@ export function Summary(){
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas"/>
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {style: 'currency', currency: "BRL"}).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saidas</p>
           <img src={outcomeImg} alt="Saidas"/>
         </header>
-        <strong>- R$500,00</strong>
+        <strong>
+          - {new Intl.NumberFormat('pt-BR', {style: 'currency', currency: "BRL"}).format(summary.withdraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total"/>
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {style: 'currency', currency: "BRL"}).format(summary.total)}
+        </strong>
       </div>
     </Container>
   )
